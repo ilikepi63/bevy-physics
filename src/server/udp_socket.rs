dd8508028199c0dbad419df3329069cc4f44c009
Cameron <cameron.harris@hpe.com>
2024-01-10 15:44:12 +0200

    Save worj

--- src/server/udp_socket.rs
+++ src/server/udp_socket.rs
@@ -1,25 +1,46 @@
 use std::{net::UdpSocket, sync::mpsc};
 use super::messages::{ClientMessages, ServerMessages};
 
-pub fn start(port: u16, client_tx: Receiver<ClientMessages>) -> std::io::Result<Receiver<ServerMessages>> {
+pub fn start(port: u16, client_rx: Receiver<ClientMessages>) -> std::io::Result<Receiver<ServerMessages>> {
 
     let (tx, rx) = mpsc::channel::<ServerMessages>();
 
-    let socket = Arc::new(Mutex::new(UdpSocket::bind(format!("127.0.0.1:{port}"))?));
+    // if this is the client, then we need to start it  as well
+    let socket = UdpSocket::bind(format!("127.0.0.1:{port}"))?;
 
-    let read_socket = socket.clone();
+    socket.set_nonblocking(true);
 
     std::thread::spawn(move || {
 
-        let mut buf = [0; 1024];
+        let mut recv_buf = [0; 1024];
+        let mut send_buf = [0; 1024];
 
-        let (amt, src) = socket.recv_from(&mut buf)?;
 
-        let buf = &mut buf[..amt];
+        loop {
 
-        buf.reverse();
+            if let Ok((amt, src)) = socket.recv_from(&mut buf) {
+                // deserialize the message and 
 
-        socket.send_to(buf, &src)?;
+                
+
+            };
+
+            if let Ok(message) = client_rx.try_recv() {
+                  // serialize the message and send to the socket
+            } 
+            
+
+        }
+
+
+
+
+
+    //     let buf = &mut buf[..amt];
+
+    //     buf.reverse();
+
+    //     socket.send_to(buf, &src)?;
     });
 
 
